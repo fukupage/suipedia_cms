bf8ac763241ce28bcbd69322a82cfa093d054f6a
fukupage <4033990+fukupage@users.noreply.github.com>
2020-07-13 11:27:27 +0900

    ＃6

--- src/common/function.php
+++ src/common/function.php
@@ -8,4 +8,27 @@ function getRoot(){
 function getRequestURL(){
   return $_SERVER["REQUEST_URI"];
 }
+
+function getDbh(){
+  $dsn = 'mysql:dbname = suipedia_cms;host=http://localhost:8888/';
+  $user = 'root';
+  $pass = '';
+  try{
+    $dbh = new PDO($dsn, $user, $pass);
+    if($dbh == null){
+      p('接続失敗');
+    } else {
+      p('接続成功');
+    }
+    $dbh -> query('SET NAMES utf8mb4');
+  } catch(PDOException $e){
+    p('Error：'.$e->getMessage());
+    p('データベースへの接続に失敗しました。');
+    die();
+  }
+  return $dbh;
+}
+function p($str){
+  print $str;
+}
 ?>
